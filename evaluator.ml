1cfaf75d8214b876447f397e8f427baac10be203
Khoa Nguyen <khoa02031995@gmail.com>
2018-07-20 01:47:21 +0700

    - Flush stderr and stdout to js output - Add Reset and syntax toggle

--- evaluator.ml
+++ evaluator.ml
@@ -1,6 +1,28 @@
-let () =
-begin
-  JsooTop.initialize ();
+module ToploopBackup = struct
+  let parse_toplevel_phrase = !Toploop.parse_toplevel_phrase
+  let print_out_value = !Toploop.print_out_value
+  let print_out_type = !Toploop.print_out_type
+  let print_out_class_type = !Toploop.print_out_class_type
+  let print_out_module_type = !Toploop.print_out_module_type
+  let print_out_type_extension = !Toploop.print_out_type_extension
+  let print_out_sig_item = !Toploop.print_out_sig_item
+  let print_out_signature = !Toploop.print_out_signature
+  let print_out_phrase = !Toploop.print_out_phrase
+end
+
+let mlSyntax () = begin 
+  Toploop.parse_toplevel_phrase := ToploopBackup.parse_toplevel_phrase;
+  Toploop.print_out_value := ToploopBackup.print_out_value;
+  Toploop.print_out_type := ToploopBackup.print_out_type;
+  Toploop.print_out_class_type := ToploopBackup.print_out_class_type;
+  Toploop.print_out_module_type := ToploopBackup.print_out_module_type;
+  Toploop.print_out_type_extension := ToploopBackup.print_out_type_extension;
+  Toploop.print_out_sig_item := ToploopBackup.print_out_sig_item;
+  Toploop.print_out_signature := ToploopBackup.print_out_signature;
+  Toploop.print_out_phrase := ToploopBackup.print_out_phrase
+end
+
+let reasonSyntax () = begin 
   let open Reason_toolchain.From_current in
   let wrap f g fmt x = g fmt (f x) in
   Toploop.parse_toplevel_phrase := Reason_util.correctly_catch_parse_errors
@@ -22,17 +44,37 @@ begin
     wrap (List.map copy_out_sig_item) Reason_oprint.print_out_signature;
   Toploop.print_out_phrase :=
     wrap copy_out_phrase Reason_oprint.print_out_phrase;
-end 
+end
 
+let setup () = JsooTop.initialize ()
+  
 let execute code =
   let code = Js.to_string code in
-  let buffer = Buffer.create 100 in
-  let formatter = Format.formatter_of_buffer buffer in
-  JsooTop.execute true formatter code;
-  Js.string (Buffer.contents buffer)
+  let evaluate_buffer = Buffer.create 100 in
+  let stderr_buffer = Buffer.create 100 in
+  let stdout_buffer = Buffer.create 100 in
 
-let () =
+  Sys_js.set_channel_flusher stderr (Buffer.add_string stderr_buffer);
+  Sys_js.set_channel_flusher stdout (Buffer.add_string stdout_buffer);
+
+  let formatter = Format.formatter_of_buffer evaluate_buffer in
+  JsooTop.execute true formatter code;
+  
+  object%js
+    val evaluate = Js.string (Buffer.contents evaluate_buffer)
+    val stderr = Js.string (Buffer.contents stderr_buffer)
+    val stdout = Js.string (Buffer.contents stdout_buffer)
+  end
+
+let () = begin
+  setup ();
+  reasonSyntax ();
+
   Js.export "evaluator" (
     object%js
       val execute = execute
+      val reset = setup
+      val reasonSyntax = reasonSyntax
+      val mlSyntax = mlSyntax
     end)
+end
